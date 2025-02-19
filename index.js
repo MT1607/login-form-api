29d0f20626727f5a9ad6b97af0521febda38506b
MT1607 <nguyengiason5@gmail.com>
2025-02-19 21:55:06 +0700

    response cookie to localhost fe

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ const {requireAuth, checkUser} = require("./middleware/authMiddleware");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',  // Cho phép FE truy cập
+    credentials: true  // Cho phép gửi cookie
+}));
+
 app.use(express.static('public'));
 app.use(express.json())
 app.use(authRouters);
 app.use(cookie());
+
 
 app.set('views', './views');
 app.set("view engine", "ejs");
