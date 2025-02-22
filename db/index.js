88127317fafdb838f6ea944a5fe612111c0f22f2
MT1607 <nguyengiason5@gmail.com>
2025-02-22 10:51:34 +0700

    update env connect to neon postgres

--- db/index.js
+++ db/index.js
@@ -8,6 +8,7 @@ const client = new Client({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }, // Kích hoạt SSL
 });
 
 client.connect()
