c0ca3bb0320cb500e877e67e8b287bf0a40833bd
MT1607 <nguyengiason5@gmail.com>
2025-03-05 14:17:29 +0700

    update sql create user to create profile

--- sql/createUser.sql
+++ sql/createUser.sql
@@ -3,6 +3,6 @@ WITH new_user AS (
         VALUES ($1, $2)
         RETURNING id
 )
-INSERT INTO profiles (user_id, first_name, last_name, date_of_birth)
-VALUES ((SELECT id FROM new_user), NULL, NULL, NULL)
+INSERT INTO profiles (user_id, first_name, last_name, avatar_url, date_of_birth)
+VALUES ((SELECT id FROM new_user), NULL, NULL, NULL, NULL)
 RETURNING user_id;
